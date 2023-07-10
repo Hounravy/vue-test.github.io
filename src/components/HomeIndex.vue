adfca218537bc4feb910de6538dd4cce39b3379c
Houn Ravy <hounravy605@gmail.com>
2023-07-10 15:28:59 +0700

    vue-test

---
+++ src/components/HomeIndex.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="content1">
+        <div class="content2">
+            <div class="text-conten">
+                <img src="@/assets/img/char.png" alt="">
+            </div>
+            <div class="text-conten2">
+                <img src="@/assets/img/char2.png" alt="">
+            </div>
+            <div class="button-text">
+                <button class="bu-bg"><img src="@/assets/img/c.png" alt=""></button>
+            </div>
+            <div class="bobo">
+                <img src="@/assets/img/bobo.png" alt="">
+            </div>
+        </div>
+        <div class="banner">
+            <img src="@/assets/img/banner.png" alt="">
+        </div>
+    </div>
+
+
+
+    <div class="girl">
+
+        <img src="@/assets/img/girl.png" alt="">
+    </div>
+    <!-- the footer of this page -->
+    <div class=" cash-foot">
+        <div class="lig">
+            <img src="@/assets/img/lig.png" alt="">
+        </div>
+        <div class="cash">
+            <img src="@/assets/img/cash.png" alt="">
+        </div>
+        <div class="cash2">
+            <img src="@/assets/img/cash2.png" alt="">
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'HomeIndex',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+  
+<style></style>
